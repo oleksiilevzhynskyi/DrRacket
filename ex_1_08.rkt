2581d7025bd86c48a516144ac252fb81734cb4a5
Alexey Levzhinsky <alex_lev89@mail.ru>
2011-09-05 07:43:11 +0300

    Small fix

--- ex_1_08.rkt
+++ ex_1_08.rkt
@@ -1,8 +1,9 @@
 #lang racket
 
+(require rackunit)
+
 (define (square x)
   (* x x))
-
 
 (define (good-enough? guess guess_old)
 (< (abs (- guess guess_old)) 0.00001))
@@ -19,11 +20,12 @@
 (define (sqrt-iter guess guess_old x)
   (if (good-enough? guess guess_old)
       guess
-      (sqrt-iter (improve guess x) 
+      (sqrt-iter (improve guess x)
                  guess
                  x)))
 
 (define (sqrt3 x)
-(sqrt-iter 1.0 1.1 x))
+  (sqrt-iter 1.0 1.1 x))
 
-(sqrt3 27.0)
+(check-equal? (sqrt3 27.0) 3)
+
